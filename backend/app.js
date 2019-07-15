ef905502554912105fd17a2442ba6fda673ef0e3
Patrick Lai <patricklai10123@gmail.com>
2019-07-14 21:46:01 -0700

    add retrieving images

--- backend/app.js
+++ backend/app.js
@@ -7,8 +7,11 @@ const router = express.Router();
 const app = express();
 const PORT = 3001;
 
+// public folder where uploaded images are stored
 app.use(express.static('public'));
+app.use('/static', express.static(path.join(__dirname, 'public')))
 
+// allow CORS from the frontend
 app.use(function(req, res, next) {
     res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   
@@ -27,6 +30,7 @@ app.use(function(req, res, next) {
     next();
   });
 
+  // store iamges on the file system of the server
   const storage = multer.diskStorage({
     destination: "./public/uploads/",
     filename: function(req, file, cb){
@@ -34,13 +38,14 @@ app.use(function(req, res, next) {
     }
  });
  
- const upload = multer({
+ // single image upload
+ const uploadSingle = multer({
     storage: storage,
     limits:{fileSize: 1000000},
  }).single("myImage");
 
  app.post('/upload', function (req, res) {
-  upload(req, res, function (err) {
+  uploadSingle(req, res, function (err) {
       console.log("Request ---", req.body);
       console.log("Request file ---", req.file);
       
@@ -50,13 +55,11 @@ app.use(function(req, res, next) {
   })
 })
 
-// app.post('/upload', upload.single('photo'), (req, res) => {
-//     if(req.file) {
-//         console.log("image received: " + req.file.originalname);
-//         res.json(req.file);
-//     }
-//     else throw 'error';
-// });
+// retrieve an image
+app.get('/getImage', function (req, res){
+  console.log(req);
+  res.send('static/uploads/IMAGE-1563083113002.png');
+})
 
 app.listen(PORT, () => {
     console.log('Image repo started. Listening at ' + PORT );
--- frontend/src/UploadImage.js
+++ frontend/src/UploadImage.js
@@ -1,36 +1,56 @@
 import React from "react";
 import Api from './services/Api';
+import './UploadImage.css';
 
 class UploadImage extends React.Component {
 
   constructor(props) {
     super(props);
+
     this.state = {
-      file: null
+      file: null,
+      images: [],
     };
+
     this.onFormSubmit = this.onFormSubmit.bind(this);
     this.onChange = this.onChange.bind(this);
+    this.retrieveImage = this.retrieveImage.bind(this);
+    this.renderImage = this.renderImage.bind(this);
+  }
+
+  retrieveImage() {
+    Api().get("/getImage").then(res => {
+      console.log(res)
+      this.setState({
+        images: [...this.state.images, res.config.baseURL + res.data]
+      })
+    })
   }
 
   onFormSubmit(e) {
     e.preventDefault();
+
     const formData = new FormData();
+
     formData.append("myImage", this.state.file);
+
     const config = {
       headers: {
         "content-type": "multipart/form-data"
       }
     };
-    console.log(formData)
-    // axios
-    //   .post("/upload", formData, config)
+
     Api().post("/upload", formData, config)
-      .then(response => {
+      .then(res => {
         console("Image successfully uploaded");
       })
       .catch(error => {
         console.log(error)
       });
+  }
+
+  renderImage(imageSrc) {
+    return (<img src={imageSrc} className="gallery"/>)
   }
 
   onChange(e) {
@@ -39,11 +59,20 @@ class UploadImage extends React.Component {
 
   render() {
     return (
-      <form onSubmit={this.onFormSubmit}>
-        <h1>File Upload</h1>
-        <input type="file" name="myImage" onChange={this.onChange} />
-        <button type="submit">Upload</button>
-      </form>
+      <div>
+        <form onSubmit={this.onFormSubmit}>
+          <h1>File Upload</h1>
+          <input type="file" name="myImage" onChange={this.onChange} />
+          <button type="submit">Upload</button>
+        </form>
+        <button onClick={this.retrieveImage}>get image</button>
+        <div>
+          <h2>My Images</h2>
+          <div className="ImageGallery">
+            {this.state.images.map(image => this.renderImage(image))}
+          </div>
+        </div>
+      </div>
     );
   }
 }
